1ab0c8be0c1ca10d355e65dfc0469ba3313fb800
chye0001 <chye0001@stud.kea.dk>
2024-05-03 14:45:30 +0200

    added UserController

---
+++ src/main/java/dev/tolana/projectcalculationtool/controller/UserController.java
@@ -0,0 +1,22 @@
+package dev.tolana.projectcalculationtool.controller;
+
+import dev.tolana.projectcalculationtool.dto.UserDto;
+import dev.tolana.projectcalculationtool.service.UserService;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+}
