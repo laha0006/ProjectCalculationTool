05930a65ca3be732e3b6defe16efa22cc9f3aa5c
chye0001 <chye0001@stud.kea.dk>
2024-05-03 14:43:11 +0200

    configured spring SecurityConfig

--- src/main/java/dev/tolana/projectcalculationtool/config/SecurityConfig.java
+++ src/main/java/dev/tolana/projectcalculationtool/config/SecurityConfig.java
@@ -1,4 +1,47 @@
 package dev.tolana.projectcalculationtool.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/user/**").authenticated())
+                .formLogin(form -> form
+                        .loginPage("/user/login")
+                        .failureUrl("/user/login?error")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/user/logout")
+                        .logoutSuccessUrl("/user/login?logout")
+                        .permitAll());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
