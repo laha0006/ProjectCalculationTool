ae56bc447452560385b8a4830f827dd652023595
chye0001 <chye0001@stud.kea.dk>
2024-05-04 17:16:10 +0200

    added method displayDashboardOnUser to IndexController

--- src/main/java/dev/tolana/projectcalculationtool/controller/IndexController.java
+++ src/main/java/dev/tolana/projectcalculationtool/controller/IndexController.java
@@ -1,4 +1,30 @@
 package dev.tolana.projectcalculationtool.controller;
 
+import dev.tolana.projectcalculationtool.dto.UserEntityRoleDto;
+import dev.tolana.projectcalculationtool.service.IndexService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
 public class IndexController {
+
+    private final IndexService indexService;
+
+    public IndexController(IndexService indexService) {
+        this.indexService = indexService;
+    }
+
+    @GetMapping("")
+    public String displayDashboardOnUser(Model model, Authentication authentication) {
+        String username = authentication.getName();
+
+        UserEntityRoleDto user = indexService.getAuthenticatedUser(username);
+        model.addAttribute("user", user);
+
+        return "index";
+    }
 }
