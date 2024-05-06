9802f0734925ef7666d0fc92c9d0ccb7cfb8ffb2
chye0001 <chye0001@stud.kea.dk>
2024-05-06 13:31:02 +0200

    configured IndexService.java

--- src/main/java/dev/tolana/projectcalculationtool/service/IndexService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/IndexService.java
@@ -1,4 +1,16 @@
 package dev.tolana.projectcalculationtool.service;
 
+import dev.tolana.projectcalculationtool.dto.UserEntityRoleDto;
+import dev.tolana.projectcalculationtool.repository.IndexRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class IndexService {
+
+    private IndexRepository indexRepository;
+    public IndexService(IndexRepository indexRepository) {
+        this.indexRepository = indexRepository;
+    }
 }
