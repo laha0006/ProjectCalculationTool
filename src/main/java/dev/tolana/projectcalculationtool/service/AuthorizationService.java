0185f2b1944210cf68b7f0e4009e7b08297ee427
Lars <laha0006@stud.kea.dk>
2024-05-06 11:37:08 +0200

    added constructor

--- src/main/java/dev/tolana/projectcalculationtool/service/AuthorizationService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/AuthorizationService.java
@@ -1,7 +1,18 @@
 package dev.tolana.projectcalculationtool.service;
 
+import dev.tolana.projectcalculationtool.repository.AuthorizationRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorizationService {
+
+    private AuthorizationRepository authorizationRepository;
+
+    public AuthorizationService(AuthorizationRepository authorizationRepository) {
+        this.authorizationRepository = authorizationRepository;
+    }
+
+    private getHierarchy(long id, AccessLevel) {
+
+    }
 }
