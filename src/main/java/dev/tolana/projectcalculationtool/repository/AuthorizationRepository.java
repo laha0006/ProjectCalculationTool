55a22d2a173fd0086eb9f42725fbc71f4085fad7
Lars <laha0006@stud.kea.dk>
2024-05-06 11:37:16 +0200

    added constructor

--- src/main/java/dev/tolana/projectcalculationtool/repository/AuthorizationRepository.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/AuthorizationRepository.java
@@ -2,7 +2,16 @@ package dev.tolana.projectcalculationtool.repository;
 
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+
 @Repository
 public class AuthorizationRepository {
 
+    private DataSource dataSource;
+
+    public AuthorizationRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    
 }
