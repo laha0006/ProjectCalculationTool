f4196b20f058e36d90078d82820b78e2febf8ee3
chye0001 <chye0001@stud.kea.dk>
2024-05-06 15:45:35 +0200

    scraped contents of IndexController

--- src/main/java/dev/tolana/projectcalculationtool/controller/IndexController.java
+++ src/main/java/dev/tolana/projectcalculationtool/controller/IndexController.java
@@ -1,22 +1,8 @@
 package dev.tolana.projectcalculationtool.controller;
 
-import dev.tolana.projectcalculationtool.dto.UserEntityRoleDto;
-import dev.tolana.projectcalculationtool.service.IndexService;
-import org.springframework.security.core.Authentication;
+import dev.tolana.projectcalculationtool.service.DashboardService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 public class IndexController {
-
-    private final IndexService indexService;
-
-    public IndexController(IndexService indexService) {
-        this.indexService = indexService;
-    }
-
 }
