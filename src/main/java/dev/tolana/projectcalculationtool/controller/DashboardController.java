5feac9fff8d509c4489bb3c207146317d548fba1
chye0001 <chye0001@stud.kea.dk>
2024-05-06 13:29:44 +0200

    refacroed and moved contents from IndexController to DashboardController

--- src/main/java/dev/tolana/projectcalculationtool/controller/DashboardController.java
+++ src/main/java/dev/tolana/projectcalculationtool/controller/DashboardController.java
@@ -1,4 +1,24 @@
 package dev.tolana.projectcalculationtool.controller;
 
+import dev.tolana.projectcalculationtool.dto.UserEntityRoleDto;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/dashboard")
 public class DashboardController {
+
+    @GetMapping("")
+    public String displayDashboardOnUser(Model model, Authentication authentication) {
+        String username = authentication.getName();
+        List<UserEntityRoleDto> userEntityRoleList = indexService.getUserEntityRoleListOnUsername(username);
+        model.addAttribute("user", userEntityRoleList);
+
+        return "index";
+    }
 }
