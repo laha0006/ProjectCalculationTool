56b14f5be70225aee029e1ba9ffd4a53b0d0e93a
chye0001 <chye0001@stud.kea.dk>
2024-05-07 12:47:00 +0200

    added endpoints for task creation in TaskController.java

--- src/main/java/dev/tolana/projectcalculationtool/controller/TaskController.java
+++ src/main/java/dev/tolana/projectcalculationtool/controller/TaskController.java
@@ -1,9 +1,16 @@
 package dev.tolana.projectcalculationtool.controller;
 
+import dev.tolana.projectcalculationtool.dto.CreateTaskFormDto;
 import dev.tolana.projectcalculationtool.service.TaskService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/task")
@@ -15,8 +22,15 @@ public class TaskController {
     }
 
     @GetMapping("/create")
-    public String createForm() {
+    public String createForm(Model model) {
+        model.addAttribute("taskDto", new CreateTaskFormDto("", "", 0, LocalDate.now()));
+        return "task/createTask";
+    }
 
-        return "createTask";
+    @PostMapping("/create")
+    public String createTask(CreateTaskFormDto newTask, Authentication authentication) {
+        String username = authentication.getName();
+        taskService.createTask(newTask, username);
+        return "redirect:/"; //TODO redirect to a summary page.
     }
 }
