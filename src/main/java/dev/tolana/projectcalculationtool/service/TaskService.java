3c87310bbbb3903d692c711d71b90865f6af5664
chye0001 <chye0001@stud.kea.dk>
2024-05-07 13:17:15 +0200

    added method createTask(CreateTaskFormDto newTask, String username) in TaskService

--- src/main/java/dev/tolana/projectcalculationtool/service/TaskService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/TaskService.java
@@ -1,5 +1,6 @@
 package dev.tolana.projectcalculationtool.service;
 
+import dev.tolana.projectcalculationtool.dto.CreateTaskFormDto;
 import dev.tolana.projectcalculationtool.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,9 @@ public class TaskService {
     private TaskRepository taskRepository;
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+    }
+
+    public void createTask(CreateTaskFormDto newTask, String username) {
+        taskRepository.createTask(newTask, username);
     }
 }
