7b9c90e130589989a447cb7af420416fff009209
chri26eq <chri26eq@stud.kea.dk>
2024-05-07 11:11:54 +0200

    added createOrganisation()
    ændret 'z' til 's'

--- src/main/java/dev/tolana/projectcalculationtool/repository/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.tolana.projectcalculationtool.repository;
-
-import dev.tolana.projectcalculationtool.model.Organization;
-
-import java.util.List;
-
-public interface OrganizationRepository {
-    List<Organization> getOrganizationsByUser(String username);
-}
---
+++ src/main/java/dev/tolana/projectcalculationtool/service/OrganisationService.java
@@ -0,0 +1,36 @@
+package dev.tolana.projectcalculationtool.service;
+
+import dev.tolana.projectcalculationtool.model.Organisation;
+import dev.tolana.projectcalculationtool.repository.OrganisationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrganisationService {
+
+    private final OrganisationRepository organisationRepository;
+
+    public OrganisationService(OrganisationRepository organisationRepository) {
+        this.organisationRepository = organisationRepository;
+    }
+
+    public List<Organisation> getOrganisationsByUser(String username) {
+        return organisationRepository.getOrganisationsByUser(username);
+    }
+
+    public List<Organisation> getNotArchivedOrganisationsByUser(String username) {
+        List<Organisation> organisations = organisationRepository.getOrganisationsByUser(username);
+        List<Organisation> notArchivedOrganisations = new ArrayList<>();
+        for (Organisation organisation : organisations) {
+            if (!organisation.isArchived()) {
+                notArchivedOrganisations.add(organisation);}
+        }
+        return notArchivedOrganisations;
+    }
+
+    public void createOrganisation(String username, String organisationName, String organisationDescription) {
+    organisationRepository.createOrganisation(username, organisationName, organisationDescription);
+    }
+}
