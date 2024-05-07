98134790b95af9528598adab8d6e145129e2a27f
chye0001 <chye0001@stud.kea.dk>
2024-05-07 22:49:44 +0200

    deleted CreateTaskFormDto.java

--- src/main/java/dev/tolana/projectcalculationtool/dto/CreateTaskFormDto.java
+++
@@ -1,9 +0,0 @@
-package dev.tolana.projectcalculationtool.dto;
-
-import java.time.LocalDate;
-
-public record CreateTaskFormDto(String taskName,
-                                String taskDescription,
-                                int estimatedHours,
-                                LocalDate deadline) {
-}
