dc566e1a75254c709c7b92bb875637d4da1152e0
chye0001 <chye0001@stud.kea.dk>
2024-05-07 13:16:08 +0200

    populated interface; TaskRepository with CRUD operations

--- src/main/java/dev/tolana/projectcalculationtool/repository/TaskRepository.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/TaskRepository.java
@@ -1,4 +1,21 @@
 package dev.tolana.projectcalculationtool.repository;
 
+import dev.tolana.projectcalculationtool.dto.CreateTaskFormDto;
+import dev.tolana.projectcalculationtool.model.Task;
+
+import java.util.List;
+
 public interface TaskRepository {
+
+    boolean createTask(CreateTaskFormDto taskFormDto, String username);
+
+    boolean deleteTask(long taskId, String username);
+
+    boolean editTask(long taskId, String username);
+
+    Task getTask(long taskId, String username);
+
+    List<Task> getAllTasks(String username);
+
+    List<Task> getAllProjectTasks(long projectId, String username);
 }
