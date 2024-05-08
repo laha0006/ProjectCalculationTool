a49d22c35d190b6a824f5f342c6fd144859881d7
Lars <laha0006@stud.kea.dk>
2024-05-08 19:06:42 +0200

    added method to get matching role ids

--- src/main/java/dev/tolana/projectcalculationtool/service/AuthorizationService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/AuthorizationService.java
@@ -5,10 +5,14 @@ import dev.tolana.projectcalculationtool.constant.Permission;
 import dev.tolana.projectcalculationtool.dto.HierarchyDto;
 import dev.tolana.projectcalculationtool.model.Role;
 import dev.tolana.projectcalculationtool.repository.AuthorizationRepository;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @Service("auth")
 public class AuthorizationService {
@@ -26,8 +30,14 @@ public class AuthorizationService {
     }
 
     public boolean hasAccess(long id, AccessLevel accessLevel, Permission permission) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
         HierarchyDto hierarchy = getHierarchy(id, accessLevel);
+        Set<Long> roleIds = getRoleIdsMatchingHierarchy(username,hierarchy,accessLevel);
         return true;
+    }
+
+    private Set<Long> getRoleIdsMatchingHierarchy(String username, HierarchyDto hierarchy, AccessLevel accessLevel) {
+        Set<Long> roleIds = authorizationRepository.getRoleIdsMatchingHierarchy(username,hierarchy,accessLevel);
     }
 
     private HierarchyDto getHierarchy(long id, AccessLevel accessLevel) {
