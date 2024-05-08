99bb37cc5c00f7ec157781b5e4721cdf215753a2
Lars <laha0006@stud.kea.dk>
2024-05-08 17:37:10 +0200

    implemented role Model

--- src/main/java/dev/tolana/projectcalculationtool/model/Role.java
+++ src/main/java/dev/tolana/projectcalculationtool/model/Role.java
@@ -1,5 +1,12 @@
 package dev.tolana.projectcalculationtool.model;
 
+import dev.tolana.projectcalculationtool.enums.Permission;
+
+import java.util.List;
+
 public class Role {
+    private long id;
     private String roleName;
+    private short weight;
+    private List<Permission> permissions;
 }
