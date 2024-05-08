e6753f679d873f8d899ab3c4e6835ab8014d9061
Lars <laha0006@stud.kea.dk>
2024-05-08 17:37:21 +0200

    added method to initRoles list.

--- src/main/java/dev/tolana/projectcalculationtool/service/AuthorizationService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/AuthorizationService.java
@@ -2,16 +2,27 @@ package dev.tolana.projectcalculationtool.service;
 
 import dev.tolana.projectcalculationtool.constant.AccessLevel;
 import dev.tolana.projectcalculationtool.dto.HierarchyDto;
+import dev.tolana.projectcalculationtool.model.Role;
 import dev.tolana.projectcalculationtool.repository.AuthorizationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service("auth")
 public class AuthorizationService {
 
     private AuthorizationRepository authorizationRepository;
+    private Map<Long, Role> roles;
 
     public AuthorizationService(AuthorizationRepository authorizationRepository) {
         this.authorizationRepository = authorizationRepository;
+        roles = initRoles();
+    }
+
+    private Map<Long, Role> initRoles() {
+        Map<Long, Role> roles = new HashMap<>();
+
     }
 
     public boolean hasAccess() {
