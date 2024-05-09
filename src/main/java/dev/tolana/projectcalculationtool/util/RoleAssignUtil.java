099c589bdf333bc101430d3049f26ea0097234d6
Lars <laha0006@stud.kea.dk>
2024-05-09 07:14:43 +0200

    updated to use UserRole enum, for cleaner usage.

--- src/main/java/dev/tolana/projectcalculationtool/util/RoleAssignUtil.java
+++ src/main/java/dev/tolana/projectcalculationtool/util/RoleAssignUtil.java
@@ -1,14 +1,12 @@
 package dev.tolana.projectcalculationtool.util;
 
 import dev.tolana.projectcalculationtool.constant.AccessLevel;
-import dev.tolana.projectcalculationtool.constant.Role;
+import dev.tolana.projectcalculationtool.constant.UserRole;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class RoleAssignUtil {
@@ -18,7 +16,7 @@ public class RoleAssignUtil {
     private static final String PROJECT_INSERT_ROLE_SQL = "INSERT INTO user_entity_role(username,role_id,project_id) VALUES(?,?,?)";
     private static final String TASK_INSERT_ROLE_SQL = "INSERT INTO user_entity_role(username,role_id,task_id) VALUES(?,?,?)";
 
-    private static int assignRole(Connection con,AccessLevel accessLevel,long id, long roleId, String username) throws SQLException {
+    private static int assignRole(Connection con,AccessLevel accessLevel,long id, UserRole userRole, String username) throws SQLException {
         String SQL = switch(accessLevel) {
             case TASK -> TASK_INSERT_ROLE_SQL;
             case PROJECT -> PROJECT_INSERT_ROLE_SQL;
@@ -28,29 +26,30 @@ public class RoleAssignUtil {
         };
         PreparedStatement ps = con.prepareStatement(SQL);
         ps.setString(1, username);
-        ps.setLong(2, roleId);
+        ps.setLong(2, userRole.getRoleId());
         ps.setLong(3, id);
+        System.out.println("PS:   " + ps);
         return ps.executeUpdate();
     }
 
-    public static int assignOrganisationRole(Connection con,long id, long roleId, String username) throws SQLException {
-        return assignRole(con,AccessLevel.ORGANISATION,id,roleId,username);
+    public static int assignOrganisationRole(Connection con, long id, UserRole userRole, String username) throws SQLException {
+        return assignRole(con,AccessLevel.ORGANISATION,id,userRole,username);
     }
 
-    public static int assignDepartmentRole(Connection con,long id, long roleId, String username) throws SQLException {
-        return assignRole(con,AccessLevel.DEPARTMENT,id,roleId,username);
+    public static int assignDepartmentRole(Connection con,long id, UserRole userRole, String username) throws SQLException {
+        return assignRole(con,AccessLevel.DEPARTMENT,id,userRole,username);
     }
 
-    public static int assignTeamRole(Connection con,long id, long roleId, String username) throws SQLException {
-        return assignRole(con,AccessLevel.TEAM,id,roleId,username);
+    public static int assignTeamRole(Connection con,long id, UserRole userRole, String username) throws SQLException {
+        return assignRole(con,AccessLevel.TEAM,id,userRole,username);
     }
 
-    public static int assignProjectRole(Connection con,long id, long roleId, String username) throws SQLException {
-        return assignRole(con,AccessLevel.PROJECT,id,roleId,username);
+    public static int assignProjectRole(Connection con,long id, UserRole userRole, String username) throws SQLException {
+        return assignRole(con,AccessLevel.PROJECT,id,userRole,username);
     }
 
-    public static int assignTaskRole(Connection con,long id, long roleId, String username) throws SQLException {
-        return assignRole(con,AccessLevel.TASK,id,roleId,username);
+    public static int assignTaskRole(Connection con,long id, UserRole userRole, String username) throws SQLException {
+        return assignRole(con,AccessLevel.TASK,id,userRole,username);
     }
 
 
