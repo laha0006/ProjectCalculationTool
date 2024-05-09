4efbc00485c88e096fcf82094172b809feb4315b
chye0001 <chye0001@stud.kea.dk>
2024-05-09 14:41:10 +0200

    added ProjectDtoMapper and populated this with method converting a list containing projects to ProjectOverviewDto list

---
+++ src/main/java/dev/tolana/projectcalculationtool/mapper/ProjectDtoMapper.java
@@ -0,0 +1,28 @@
+package dev.tolana.projectcalculationtool.mapper;
+
+import dev.tolana.projectcalculationtool.dto.ProjectOverviewDto;
+import dev.tolana.projectcalculationtool.model.Project;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ProjectDtoMapper {
+
+    public List<ProjectOverviewDto> toProjectOverviewDtoList(List<Project> projectList) {
+        List<ProjectOverviewDto> projectOverviewDtoList = new ArrayList<>();
+
+        for (Project project : projectList) {
+            ProjectOverviewDto projectOverviewDto = new ProjectOverviewDto(
+                    project.getName(),
+                    project.getDeadline(),
+                    project.getAllottedHours(),
+                    project.getStatus()
+            );
+            projectOverviewDtoList.add(projectOverviewDto);
+        }
+
+        return projectOverviewDtoList;
+    }
+}
