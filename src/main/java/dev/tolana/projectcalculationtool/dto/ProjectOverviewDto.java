9a6031e86705d684a4436ea90ffc8686f79bcfc1
chye0001 <chye0001@stud.kea.dk>
2024-05-09 14:41:22 +0200

    added ProjectOverviewDto

---
+++ src/main/java/dev/tolana/projectcalculationtool/dto/ProjectOverviewDto.java
@@ -0,0 +1,10 @@
+package dev.tolana.projectcalculationtool.dto;
+
+import java.sql.Timestamp;
+
+public record ProjectOverviewDto (
+        String name,
+        Timestamp deadline,
+        int allottedHours,
+        int status) {
+}
