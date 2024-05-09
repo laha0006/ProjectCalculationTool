22ef65dd99ebea9d33fa4de19034ed5e5ea33ce0
Lars <laha0006@stud.kea.dk>
2024-05-09 07:15:23 +0200

    created beforeEach method to setup a connection for testing.

--- src/test/java/dev/tolana/projectcalculationtool/service/AuthorizationServiceTests.java
+++ src/test/java/dev/tolana/projectcalculationtool/service/AuthorizationServiceTests.java
@@ -3,10 +3,19 @@ package dev.tolana.projectcalculationtool.service;
 
 import dev.tolana.projectcalculationtool.constant.AccessLevel;
 import dev.tolana.projectcalculationtool.constant.Permission;
+import dev.tolana.projectcalculationtool.constant.UserRole;
+import dev.tolana.projectcalculationtool.util.RoleAssignUtil;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,6 +25,16 @@ public class AuthorizationServiceTests {
 
     @Autowired
     private AuthorizationService auth;
+
+    @Autowired
+    private DataSource dataSource;
+
+    private Connection con;
+
+    @BeforeEach
+    public void setUp() throws SQLException {
+        con = dataSource.getConnection();
+    }
 
     @WithMockUser("masiomasu")
     @Test
@@ -50,5 +69,12 @@ public class AuthorizationServiceTests {
         assertTrue(auth.hasAccess(1, AccessLevel.TEAM, Permission.READ));
         assertTrue(auth.hasAccess(1, AccessLevel.PROJECT, Permission.READ));
         assertTrue(auth.hasAccess(1, AccessLevel.TASK, Permission.READ));
+    }
+
+    @WithMockUser("TheNewGuy")
+    @Test
+    public void assignRole() throws SQLException {
+        RoleAssignUtil.assignDepartmentRole(con,1,UserRole.ADMIN,"TheNewGuy");  // admin of Dept 1
+        assertFalse(auth.hasAccess(1, AccessLevel.DEPARTMENT, Permission.DELETE));
     }
 }
