f289696541c53011ffe23b02036f60de595d65a4
chye0001 <chye0001@stud.kea.dk>
2024-05-09 13:15:19 +0200

    changed attribute name to match camelcase convention

--- src/main/java/dev/tolana/projectcalculationtool/model/Project.java
+++ src/main/java/dev/tolana/projectcalculationtool/model/Project.java
@@ -4,21 +4,26 @@ import java.sql.Timestamp;
 
 public class Project {
     private String name, description;
-    private int team_id, parent_id, allotted_hours;
-    private int status; //TODO make into Enum
+    private long teamId, parentId, projectId;
+    private int allottedHours, status; //TODO make into Enum
     private Timestamp deadline;
 
     public Project(){}
 
-    public Project(String name, String description, int team_id, Timestamp deadline,
-                   int allotted_hours, int status, int parent_id){
+    public Project(String name,
+                   String description,
+                   long teamId,
+                   Timestamp deadline,
+                   int allottedHours,
+                   int status,
+                   long parentId){
         this.name = name;
         this.description = description;
-        this.team_id = team_id;
+        this.teamId = teamId;
         this.deadline = deadline;
-        this.allotted_hours = allotted_hours;
+        this.allottedHours = allottedHours;
         this.status = status;
-        this.parent_id = parent_id;
+        this.parentId = parentId;
     }
 
     public String getName() {
@@ -37,28 +42,28 @@ public class Project {
         this.description = description;
     }
 
-    public int getTeam_id() {
-        return team_id;
+    public long getTeamId() {
+        return teamId;
     }
 
-    public void setTeam_id(int team_id) {
-        this.team_id = team_id;
+    public void setTeamId(long teamId) {
+        this.teamId = teamId;
     }
 
-    public int getParent_id() {
-        return parent_id;
+    public long getParentId() {
+        return parentId;
     }
 
-    public void setParent_id(int parent_id) {
-        this.parent_id = parent_id;
+    public void setParentId(long parentId) {
+        this.parentId = parentId;
     }
 
-    public int getAllotted_hours() {
-        return allotted_hours;
+    public int getAllottedHours() {
+        return allottedHours;
     }
 
-    public void setAllotted_hours(int allotted_hours) {
-        this.allotted_hours = allotted_hours;
+    public void setAllottedHours(int allottedHours) {
+        this.allottedHours = allottedHours;
     }
 
     public int getStatus() {
@@ -75,5 +80,13 @@ public class Project {
 
     public void setDeadline(Timestamp deadline) {
         this.deadline = deadline;
+    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(long projectId) {
+        this.projectId = projectId;
     }
 }
