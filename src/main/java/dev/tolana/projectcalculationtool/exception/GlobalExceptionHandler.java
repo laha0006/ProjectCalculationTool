f550ddac9575d03d3eb9f25963579a19e3fa82f7
Lars <laha0006@stud.kea.dk>
2024-05-10 06:17:45 +0200

    added GlobalExceptionHandler

--- src/main/java/dev/tolana/projectcalculationtool/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/tolana/projectcalculationtool/exception/GlobalExceptionHandler.java
@@ -1,17 +1,30 @@
 package dev.tolana.projectcalculationtool.exception;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.SQLException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+//    this exception is thrown by JdbcUserDetailsManager
+//    which is the only class that will ever throw it.
+//    we know it's a 'username already exists' error, and send that back.
+    @ExceptionHandler(DuplicateKeyException.class)
+    public String sqlException(SQLException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("error", "username already exsits!");
+        return "redirect:/user/register";
+    }
 
-    @ExceptionHandler(SQLException.class)
-    public String sqlException(SQLException ex) {
-        System.out.println("SQLException: " + ex.getMessage());
-        return "user/dashboard";
+    @ExceptionHandler(MyCustomException.class)
+    public String myCustomException(MyCustomException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        System.out.println("MyCustomException: " + ex.getMessage());
+        String referer = request.getHeader("Referer");
+        return "redirect:" + referer;
     }
 }
