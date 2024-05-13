c9f2d39583b5d6f286fef791b9c03b112555e454
chye0001 <chye0001@stud.kea.dk>
2024-05-13 21:43:55 +0200

    made test for TaskController endpoints

--- src/test/java/dev/tolana/projectcalculationtool/controller/TaskControllerTest.java
+++ src/test/java/dev/tolana/projectcalculationtool/controller/TaskControllerTest.java
@@ -1,5 +1,6 @@
 package dev.tolana.projectcalculationtool.controller;
 
+import dev.tolana.projectcalculationtool.dto.TaskDto;
 import dev.tolana.projectcalculationtool.service.TaskService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -36,7 +39,20 @@ class TaskControllerTest {
     @Test
     @WithMockUser()
     void createTask() throws Exception {
-        mockMvc.perform(post("/task/create").with(csrf()))
+        long projectId = 1;
+        TaskDto newTask = new TaskDto("", "", projectId, LocalDateTime.now(), 0, 0, 0 , 0);
+
+        mockMvc.perform(post("/task/create")
+                        .with(csrf())
+                        .param("taskName","1")
+                        .param("taskDescription","1")
+                        .param("projectId","1")
+                        .param("deadline","2024-05-01T10:32")
+                        .param("estimatedHours","1")
+                        .param("status","1")
+                        .param("parentId","1")
+                        .param("taskId","1")
+                )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/task/overview/1"));
     }
