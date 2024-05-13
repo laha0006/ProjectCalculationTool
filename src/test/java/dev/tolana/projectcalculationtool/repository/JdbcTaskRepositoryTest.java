03c2d3df94ba3f8f2af20e3e2626d250292ccc43
chye0001 <chye0001@stud.kea.dk>
2024-05-13 11:41:03 +0200

    added JdbcTaskRepositoryTest and populated this with all corresponding test not including logic

---
+++ src/test/java/dev/tolana/projectcalculationtool/repository/JdbcTaskRepositoryTest.java
@@ -0,0 +1,32 @@
+package dev.tolana.projectcalculationtool.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JdbcTaskRepositoryTest {
+
+    @Test
+    void createParentTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void editTask() {
+    }
+
+    @Test
+    void getTask() {
+    }
+
+    @Test
+    void getAllTasks() {
+    }
+
+    @Test
+    void getAllProjectTasks() {
+    }
+}
