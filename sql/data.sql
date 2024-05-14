e048d10062b91e3373dc21463694d6b82b699141
Lars <laha0006@stud.kea.dk>
2024-05-14 13:11:42 +0200

    added 3 statuses in data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -13,9 +13,24 @@ VALUES ('OWNER', 255),
        ('USER', 1);
 
 INSERT INTO role_permission(role_id, perm_id)
-VALUES (1, 1),(1, 2),(1, 3),(1, 4),(1, 5),(1, 6),  # owner
-       (2, 1),(2, 2),(2, 3),(2, 4),(2, 5),(2, 6),  # admin
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6), # owner
+       (2, 1),
+       (2, 2),
+       (2, 3),
+       (2, 4),
+       (2, 5),
+       (2, 6), # admin
        (3, 3) # member
+
+INSERT INTO status(name)
+VALUES ("I gang"),
+       ("Afsluttet"),
+       ("Til gennemsyn")
 
 
 
