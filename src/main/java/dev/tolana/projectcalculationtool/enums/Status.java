48de09a59141d0097e042c1fafd0b3d0b4c4e4c9
Lars <laha0006@stud.kea.dk>
2024-05-14 15:16:50 +0200

    implemented Status enum with id and text fields

--- src/main/java/dev/tolana/projectcalculationtool/enums/Status.java
+++ src/main/java/dev/tolana/projectcalculationtool/enums/Status.java
@@ -1,16 +1,22 @@
 package dev.tolana.projectcalculationtool.enums;
 
 public enum Status {
-    IN_PROGRESS("I gang"),
-    DONE("Afsluttet"),
-    IN_REVIEW("Til gennemsyn");
+    IN_PROGRESS(1,"I gang"),
+    DONE(2,"Afsluttet"),
+    IN_REVIEW(3,"Til gennemsyn");
 
+    private final long id;
     private final String text;
-    Status(String text) {
+    Status(long id, String text) {
         this.text = text;
+        this.id = id;
     }
     public String getText() {
         return text;
+    }
+
+    public long getId() {
+        return id;
     }
 
 }
