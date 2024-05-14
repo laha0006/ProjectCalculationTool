8f6f63c61024ef977a658ed6c94602c39642e17f
Lars <laha0006@stud.kea.dk>
2024-05-14 21:36:46 +0200

    created DepartmentController

---
+++ src/main/java/dev/tolana/projectcalculationtool/controller/DepartmentController.java
@@ -0,0 +1,17 @@
+package dev.tolana.projectcalculationtool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/organisation/{org_id}/department")
+public class DepartmentController {
+
+    @GetMapping("/{dept_id}")
+    public String department(@PathVariable int dept_id,@PathVariable int org_id, Model model) {
+        return ""
+    }
+}
