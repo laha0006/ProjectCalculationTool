2f047cd6bf9160cb697d34ff205860e6b8038787
chye0001 <chye0001@stud.kea.dk>
2024-05-14 21:34:23 +0200

    created super class; HierarchicalEntity

---
+++ src/main/java/dev/tolana/projectcalculationtool/model/HierarchicalEntity.java
@@ -0,0 +1,40 @@
+package dev.tolana.projectcalculationtool.model;
+
+import java.time.LocalDateTime;
+
+public abstract class HierarchicalEntity {
+
+    private final long id;
+    private final String name;
+    private final String description;
+    private final LocalDateTime dateCreated;
+    private final boolean archived;
+
+    public HierarchicalEntity(long id, String name, String description, LocalDateTime dateCreated, boolean archived) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.dateCreated = dateCreated;
+        this.archived = archived;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+}
