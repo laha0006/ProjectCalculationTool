08475c324e017ef75759554be90751df5ad84ea2
Lars <laha0006@stud.kea.dk>
2024-05-15 03:41:57 +0200

    added security to the getAll service method

--- src/main/java/dev/tolana/projectcalculationtool/service/DepartmentService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/DepartmentService.java
@@ -2,7 +2,11 @@ package dev.tolana.projectcalculationtool.service;
 
 import dev.tolana.projectcalculationtool.model.Department;
 import dev.tolana.projectcalculationtool.repository.DepartmentRepository;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DepartmentService {
@@ -15,5 +19,16 @@ public class DepartmentService {
 
     public Department getDepartment(int deptId) {
         return departmentRepository.getDepartment(deptId);
+    }
+
+
+    @PreAuthorize("@auth.hasAccess(#id," +
+                  "T(dev.tolana.projectcalculationtool.enums.AccessLevel).ORGANISATION," +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).READ)")
+    @PostFilter("@auth.hasAccess(filterObject.id," +
+                  "T(dev.tolana.projectcalculationtool.enums.AccessLevel).DEPARTMENT," +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).READ)")
+    public List<Department> getAll(long id) {
+        return departmentRepository.getAll(id);
     }
 }
