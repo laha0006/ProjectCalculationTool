486901a71f9564146a8d7798370ce60328789fdb
Lars <laha0006@stud.kea.dk>
2024-05-15 12:26:51 +0200

    made hasAccess private, and made semantic methods, to save AccessLevel argument

--- src/main/java/dev/tolana/projectcalculationtool/service/AuthorizationService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/AuthorizationService.java
@@ -8,6 +8,7 @@ import dev.tolana.projectcalculationtool.dto.UserEntityRoleDto;
 import dev.tolana.projectcalculationtool.dto.WeightedPermissionSetDto;
 import dev.tolana.projectcalculationtool.model.Role;
 import dev.tolana.projectcalculationtool.repository.AuthorizationRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,7 @@ import java.util.*;
 @Service("auth")
 public class AuthorizationService {
 
+
     private AuthorizationRepository authorizationRepository;
     private Map<Long, Role> roles;
 
@@ -24,46 +26,69 @@ public class AuthorizationService {
         this.authorizationRepository = authorizationRepository;
     }
 
+    public boolean hasOrgansiationAccess(long organisationId, Permission requiredPermission) {
+        return hasAccess(organisationId, AccessLevel.ORGANISATION, requiredPermission);
+    }
+
+    public boolean hasDepartmentAccess(long departmentId, Permission requiredPermission) {
+        return hasAccess(departmentId, AccessLevel.DEPARTMENT, requiredPermission);
+    }
+
+    public boolean hasTeamAccess(long teamId, Permission requiredPermission) {
+        return hasAccess(teamId, AccessLevel.TEAM, requiredPermission);
+    }
+
+    public boolean hasProjectAccess(long projectId, Permission requiredPermission) {
+        return hasAccess(projectId, AccessLevel.PROJECT, requiredPermission);
+    }
+
+    public boolean hasTaskAccess(long taskId, Permission requiredPermission) {
+        return hasAccess(taskId, AccessLevel.TASK, requiredPermission);
+    }
+
     private void initRoles() {
         roles = authorizationRepository.getRoles();
     }
 
-    public boolean hasAccess(long id, Permission permission) {
+    private boolean hasAccess(long entityId, AccessLevel accessLevel, Permission requiredPermission) {
         if (roles == null) {
             initRoles();
         }
+        //currently logged in user, gotten from session.
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        HierarchyDto hierarchy = authorizationRepository.getHierarchy(id);
-        List<Long> userRoleIds = authorizationRepository.getRoleIdsMatchingHierarchy(username, hierarchy);;
 
-        return hasPermission(userRoleIds, permission);
+        HierarchyDto hierarchy = authorizationRepository.getHierarchy(entityId, accessLevel);
+        List<Long> userRoleIds = authorizationRepository.getRoleIdsMatchingHierarchy(username, hierarchy);
+        ;
+
+        return hasPermission(userRoleIds, requiredPermission);
     }
 
-    private boolean hasPermission(List<Long> userRoleIds, Permission permission) {
+    private boolean hasPermission(List<Long> userRoleIds, Permission requiredPermission) {
         Set<Permission> permissions = new HashSet<>();
         for (Long roleId : userRoleIds) {
             permissions.addAll(roles.get(roleId).getPermissions());
         }
-        return permissions.contains(permission);
+        return permissions.contains(requiredPermission); // O(1)
     }
 
-    public boolean canPerform(long id, String targetUser,Permission permission) {
-        if (roles == null) {
-            initRoles();
-        }
-        String userPerformingAction = SecurityContextHolder.getContext().getAuthentication().getName();
-        HierarchyDto hierarchy = authorizationRepository.getHierarchy(id);
-        List<Long> userPerformingActionRoleIds = authorizationRepository.getRoleIdsMatchingHierarchy(userPerformingAction, hierarchy);
-        System.out.println("userPerformingAction: " + userPerformingActionRoleIds);
-        List<Long> targetUserRoleIds = authorizationRepository.getRoleIdsMatchingHierarchy(targetUser, hierarchy);
-        System.out.println("targetUserRoleIds: " + targetUserRoleIds);
-
-        if (!hasPermission(userPerformingActionRoleIds,permission)) {
-            return false;
-        }
-        return getHighestWeight(userPerformingActionRoleIds) > getHighestWeight(targetUserRoleIds);
-
-    }
+//    public boolean canPerform(long id, String targetUser, Permission permission) {
+//        if (roles == null) {
+//            initRoles();
+//        }
+//        String userPerformingAction = SecurityContextHolder.getContext().getAuthentication().getName();
+//        HierarchyDto hierarchy = authorizationRepository.getHierarchy(id);
+//        List<Long> userPerformingActionRoleIds = authorizationRepository.getRoleIdsMatchingHierarchy(userPerformingAction, hierarchy);
+//        System.out.println("userPerformingAction: " + userPerformingActionRoleIds);
+//        List<Long> targetUserRoleIds = authorizationRepository.getRoleIdsMatchingHierarchy(targetUser, hierarchy);
+//        System.out.println("targetUserRoleIds: " + targetUserRoleIds);
+//
+//        if (!hasPermission(userPerformingActionRoleIds, permission)) {
+//            return false;
+//        }
+//        return getHighestWeight(userPerformingActionRoleIds) > getHighestWeight(targetUserRoleIds);
+//
+//    }
 
     private int getHighestWeight(List<Long> userRoleIds) {
         short highestWeight = 0;
