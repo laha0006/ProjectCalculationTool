ee908bec4a75248340cfd91c6212b3d05864ce3c
chye0001 <chye0001@stud.kea.dk>
2024-05-15 16:23:57 +0200

    deleted TaskRepository

--- src/main/java/dev/tolana/projectcalculationtool/mapper/EntityDtoMapper.java
+++ src/main/java/dev/tolana/projectcalculationtool/mapper/EntityDtoMapper.java
@@ -1,8 +1,12 @@
 package dev.tolana.projectcalculationtool.mapper;
 
 import dev.tolana.projectcalculationtool.dto.EntityCreationDto;
+import dev.tolana.projectcalculationtool.dto.TaskDto;
+import dev.tolana.projectcalculationtool.enums.Status;
 import dev.tolana.projectcalculationtool.model.Entity;
 import dev.tolana.projectcalculationtool.model.Organisation;
+import dev.tolana.projectcalculationtool.model.ResourceEntity;
+import dev.tolana.projectcalculationtool.model.Task;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -35,5 +39,52 @@ public class EntityDtoMapper {
             organisationList.add(organisation);
         }
         return organisationList;
+    }
+
+    public TaskDto convertToDto(Task task) {
+
+        String taskName = task.getName();
+        String taskDescription = task.getDescription();
+        long projectId = task.getProjectId();
+        LocalDateTime deadline = task.getDeadline();
+        int estimatedHours = task.getEstimatedHours();
+        Status status = task.getStatus();
+        long parentId = task.getParentId();
+        long taskId = task.getId();
+
+        return new TaskDto(taskName, taskDescription, projectId, deadline, estimatedHours, status, parentId, taskId);
+    }
+
+    public Entity convertToEntity(TaskDto taskDto) {
+        return new Task(
+                taskDto.taskId(),
+                taskDto.taskName(),
+                taskDto.taskDescription(),
+                taskDto.deadline(),
+                taskDto.status(),
+                taskDto.parentId(),
+                taskDto.projectId(),
+                taskDto.estimatedHours()
+        );
+    }
+
+    public List<TaskDto> toTaskDtoList(List<Entity> taskList) {
+        List<TaskDto> taskDtoList = new ArrayList<>();
+
+        for (Entity task : taskList) {
+            TaskDto taskDto = new TaskDto(
+                    task.getName(),
+                    task.getDescription(),
+                    ((Task) task).getProjectId(),
+                    ((ResourceEntity) task).getDeadline(),
+                    ((Task) task).getEstimatedHours(),
+                    ((ResourceEntity) task).getStatus(),
+                    ((ResourceEntity)task).getParentId(),
+                    task.getId()
+            );
+            taskDtoList.add(taskDto);
+        }
+
+        return taskDtoList;
     }
 }
