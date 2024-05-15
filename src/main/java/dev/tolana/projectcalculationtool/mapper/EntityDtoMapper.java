1d294b48f8c9ea31a2c67cecb048abb76d625002
chye0001 <chye0001@stud.kea.dk>
2024-05-15 09:09:13 +0200

    added mapping of entityList to OrganisationList in EntityDtoMapper

--- src/main/java/dev/tolana/projectcalculationtool/mapper/EntityDtoMapper.java
+++ src/main/java/dev/tolana/projectcalculationtool/mapper/EntityDtoMapper.java
@@ -1,23 +1,39 @@
 package dev.tolana.projectcalculationtool.mapper;
 
-import dev.tolana.projectcalculationtool.dto.EntityFormDto;
+import dev.tolana.projectcalculationtool.dto.EntityCreationDto;
 import dev.tolana.projectcalculationtool.model.Entity;
 import dev.tolana.projectcalculationtool.model.Organisation;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class EntityDtoMapper {
 
-    public Entity toEntity(EntityFormDto entityFormDto) {
+    public Entity toEntity(EntityCreationDto entityCreationDto) {
         return new Entity(
                 -1,
-                entityFormDto.entityName(),
-                entityFormDto.description(),
+                entityCreationDto.entityName(),
+                entityCreationDto.description(),
                 LocalDateTime.now(),
                 true
         );
     }
 
+    public List<Organisation> toOrganisationList(List<Entity> entityList) {
+        List<Organisation> organisationList = new ArrayList<>();
+        for (Entity entity : entityList) {
+            Organisation organisation = new Organisation(
+                    entity.getId(),
+                    entity.getName(),
+                    entity.getDescription(),
+                    entity.getDateCreated(),
+                    entity.isArchived()
+            );
+            organisationList.add(organisation);
+        }
+        return organisationList;
+    }
 }
