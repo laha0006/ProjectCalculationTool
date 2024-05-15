c0b3b7a6fd91174c602761daedeb14c7bc2990e7
chye0001 <chye0001@stud.kea.dk>
2024-05-15 12:56:11 +0200

    made Project and Task extend ResourceEntity

--- src/main/java/dev/tolana/projectcalculationtool/model/Task.java
+++ src/main/java/dev/tolana/projectcalculationtool/model/Task.java
@@ -1,84 +1,30 @@
 package dev.tolana.projectcalculationtool.model;
 
+import dev.tolana.projectcalculationtool.enums.Status;
+
 import java.time.LocalDateTime;
 
-public class Task {
+public class Task extends ResourceEntity {
 
-    private long taskId;
-    private String taskName;
-    private String taskDescription;
     private long projectId;
-    private LocalDateTime dateCreated;
-    private LocalDateTime deadline;
     private int estimatedHours;
-    private int status;
-    private long parentId;
-    private boolean isArchived;
+    private int actualHours;
 
-    public Task(long taskId,
-                String taskName,
-                String taskDescription,
-                long projectId,
+    public Task(long id,
+                String name,
+                String description,
                 LocalDateTime dateCreated,
+                boolean archived,
                 LocalDateTime deadline,
-                int estimatedHours,
-                int status,
+                Status status,
                 long parentId,
-                boolean isArchived) {
-
-        this.taskId = taskId;
-        this.taskName = taskName;
-        this.taskDescription = taskDescription;
-        this.projectId = projectId;
-        this.dateCreated = dateCreated;
-        this.deadline = deadline;
-        this.estimatedHours = estimatedHours;
-        this.status = status;
-        this.parentId = parentId;
-        this.isArchived = isArchived;
-    }
-
-    public Task(String taskName,
-                String taskDescription,
                 long projectId,
-                LocalDateTime deadline,
                 int estimatedHours,
-                int status,
-                long parentId,
-                long taskId) {
-
-        this.taskName = taskName;
-        this.taskDescription = taskDescription;
+                int actualHours) {
+        super(id, name, description, dateCreated, archived, deadline, status, parentId);
         this.projectId = projectId;
-        this.deadline = deadline;
         this.estimatedHours = estimatedHours;
-        this.status = status;
-        this.parentId = parentId;
-        this.taskId = taskId;
-    }
-
-    public long getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(long taskId) {
-        this.taskId = taskId;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public String getTaskDescription() {
-        return taskDescription;
-    }
-
-    public void setTaskDescription(String taskDescription) {
-        this.taskDescription = taskDescription;
+        this.actualHours = actualHours;
     }
 
     public long getProjectId() {
@@ -89,22 +35,6 @@ public class Task {
         this.projectId = projectId;
     }
 
-    public LocalDateTime getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(LocalDateTime dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    public LocalDateTime getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(LocalDateTime deadline) {
-        this.deadline = deadline;
-    }
-
     public int getEstimatedHours() {
         return estimatedHours;
     }
@@ -113,27 +43,11 @@ public class Task {
         this.estimatedHours = estimatedHours;
     }
 
-    public int getStatus() {
-        return status;
+    public int getActualHours() {
+        return actualHours;
     }
 
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public long getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(long parentId) {
-        this.parentId = parentId;
-    }
-
-    public boolean isArchived() {
-        return isArchived;
-    }
-
-    public void setArchived(boolean archived) {
-        isArchived = archived;
+    public void setActualHours(int actualHours) {
+        this.actualHours = actualHours;
     }
 }
