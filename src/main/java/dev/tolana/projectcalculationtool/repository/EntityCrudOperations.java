9cbe0e1529dee26af77608f39ae14b3083ab5621
chye0001 <chye0001@stud.kea.dk>
2024-05-15 14:00:47 +0200

    added 2 methods List<Entity> getAllEntitiesOnId(long entityId); & boolean assignUser(long entityId, String username, UserRole role);

--- src/main/java/dev/tolana/projectcalculationtool/repository/EntityCrudOperations.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/EntityCrudOperations.java
@@ -1,5 +1,6 @@
 package dev.tolana.projectcalculationtool.repository;
 
+import dev.tolana.projectcalculationtool.enums.UserRole;
 import dev.tolana.projectcalculationtool.model.Entity;
 
 import java.util.List;
@@ -11,6 +12,8 @@ public interface EntityCrudOperations {
     Entity getEntityOnId(long id);
     List<Entity> getAllEntitiesOnUsername(String username);
 
+    List<Entity> getAllEntitiesOnId(long entityId);
+
     boolean editEntity(Entity entity);
 
     boolean deleteEntity(long entityId);
@@ -18,4 +21,6 @@ public interface EntityCrudOperations {
     boolean inviteToEntity(String inviteeUsername);
 
     boolean archiveEntity(long entityId, boolean isArchived);
+
+    boolean assignUser(long entityId, String username, UserRole role);
 }
