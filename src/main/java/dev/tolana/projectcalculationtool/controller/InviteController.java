208be03909a1661191c64ed03e5ecd12f5153da0
Lars <laha0006@stud.kea.dk>
2024-05-16 14:24:24 +0200

    made basic endpoints

--- src/main/java/dev/tolana/projectcalculationtool/controller/InviteController.java
+++ src/main/java/dev/tolana/projectcalculationtool/controller/InviteController.java
@@ -1,7 +1,21 @@
 package dev.tolana.projectcalculationtool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/invitations")
 public class InviteController {
+
+    @GetMapping("")
+    public String invitations() {
+        return "invitations";
+    }
+
+    @PostMapping("/accept")
+    public String accept() {
+        return "accept";
+    }
 }
