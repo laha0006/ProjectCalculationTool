30dc1e654a5551e7d6703f2d2c7fd75dc0ce7771
chye0001 <chye0001@stud.kea.dk>
2024-05-16 19:54:06 +0200

    implemented DepartmentRepository

--- src/main/java/dev/tolana/projectcalculationtool/repository/JdbcDepartmentRepository.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/JdbcDepartmentRepository.java
@@ -1,8 +1,10 @@
 package dev.tolana.projectcalculationtool.repository;
 
 import dev.tolana.projectcalculationtool.dto.CreateDepartmentFormDto;
+import dev.tolana.projectcalculationtool.dto.UserInformationDto;
 import dev.tolana.projectcalculationtool.enums.UserRole;
 import dev.tolana.projectcalculationtool.model.Department;
+import dev.tolana.projectcalculationtool.model.Entity;
 import dev.tolana.projectcalculationtool.util.RoleAssignUtil;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Repository;
@@ -22,48 +24,8 @@ public class JdbcDepartmentRepository implements DepartmentRepository {
     }
 
     @Override
-    public Department getDepartmentById(int deptId) {
-        Department department = null;
-        try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from department where id = ?");
-            preparedStatement.setInt(1, deptId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                department = new Department(
-                        resultSet.getLong(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3)
-                );
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return department;
-    }
-
-    @Override
-    public List<Department> getDepartmentsByOrganisationId(long id) {
-        List<Department> departments = new ArrayList<>();
-        try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from department where organisation_id = ?");
-            preparedStatement.setLong(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                departments.add(new Department(
-                        resultSet.getLong(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3)
-                ));
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return departments;
-    }
-
-    @Override
-    public void createDepartment(CreateDepartmentFormDto departmentFormDto, String username) {
+    public boolean createEntity(String username, Entity entity) {
+        boolean isCreated = false;
 
         try (Connection connection = dataSource.getConnection()) {
             try {
@@ -71,11 +33,12 @@ public class JdbcDepartmentRepository implements DepartmentRepository {
 
                 String createDepartment = "INSERT INTO department(name, description, organisation_id) VALUES (?, ?, ?)";
                 PreparedStatement pstmtAdd = connection.prepareStatement(createDepartment, Statement.RETURN_GENERATED_KEYS);
-                pstmtAdd.setString(1, departmentFormDto.deptName());
-                pstmtAdd.setString(2, departmentFormDto.deptDescription());
-                pstmtAdd.setLong(3, departmentFormDto.organisationId());
+                pstmtAdd.setString(1, entity.getName());
+                pstmtAdd.setString(2, entity.getDescription());
+                pstmtAdd.setLong(3, ((Department)entity).getOrganisationId());
+                int affectedRows = pstmtAdd.executeUpdate();
+                isCreated = affectedRows > 0;
 
-                pstmtAdd.executeUpdate();
                 ResultSet rs = pstmtAdd.getGeneratedKeys();
                 if(rs.next()) {
                     long departmentId = rs.getLong(1);
@@ -92,12 +55,93 @@ public class JdbcDepartmentRepository implements DepartmentRepository {
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
-
-
-
-
-
+        return isCreated;
     }
 
+    @Override
+    public Entity getEntityOnId(long deptId) {
+        Entity department = null;
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM department WHERE id = ?");
+            preparedStatement.setLong(1, deptId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                department = new Department(
+                        resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getTimestamp(5).toLocalDateTime(),
+                        resultSet.getBoolean(6),
+                        resultSet.getLong(4)
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return department;
+    }
 
+    @Override
+    public List<Entity> getAllEntitiesOnUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public List<Entity> getAllEntitiesOnId(long organisationId) {
+        List<Entity> departments = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM department WHERE organisation_id = ?");
+            preparedStatement.setLong(1, organisationId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                departments.add(new Department(
+                        resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getTimestamp(5).toLocalDateTime(),
+                        resultSet.getBoolean(6),
+                        resultSet.getLong(4)
+                ));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return departments;
+    }
+
+    @Override
+    public boolean editEntity(Entity entity) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteEntity(long entityId) {
+        return false;
+    }
+
+    @Override
+    public boolean inviteToEntity(String inviteeUsername) {
+        return false;
+    }
+
+    @Override
+    public boolean archiveEntity(long entityId, boolean isArchived) {
+        return false;
+    }
+
+    @Override
+    public boolean assignUser(long entityId, List<String> username, UserRole role) {
+        return false;
+    }
+
+    @Override
+    public List<UserInformationDto> getUsersFromEntityId(long entityId) {
+        return null;
+    }
+
+    @Override
+    public List<UserRole> getAllUserRoles() {
+        return null;
+    }
 }
