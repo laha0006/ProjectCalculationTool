e9d754b539621ade75967abd808cb5a4e50219c9
chri26eq <chri26eq@stud.kea.dk>
2024-05-16 12:43:36 +0200

    DepartmentRepository interface oprettet
    added createDepartment()

--- src/main/java/dev/tolana/projectcalculationtool/repository/DepartmentRepository.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/DepartmentRepository.java
@@ -1,61 +1,17 @@
 package dev.tolana.projectcalculationtool.repository;
 
+import dev.tolana.projectcalculationtool.dto.CreateDepartmentFormDto;
 import dev.tolana.projectcalculationtool.model.Department;
-import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-@Repository
-public class DepartmentRepository {
+public interface DepartmentRepository {
 
-    private DataSource dataSource;
+    Department getDepartmentById(int deptId);
 
-    public DepartmentRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    List<Department> getDepartmentsByOrganisationId(long id);
 
-    public Department getDepartment(int deptId) {
-        Department department = null;
-        try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from department where id = ?");
-            preparedStatement.setInt(1, deptId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                department = new Department(
-                        resultSet.getLong(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3)
-                );
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return department;
-    }
+    void createDepartment(CreateDepartmentFormDto departmentFormDto, String username);
 
-    public List<Department> getAll(long id) {
-        List<Department> departments = new ArrayList<>();
-        try(Connection connection = dataSource.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from department where organisation_id = ?");
-            preparedStatement.setLong(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                departments.add(new Department(
-                        resultSet.getLong(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3)
-                ));
-            }
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return departments;
-    }
 }
