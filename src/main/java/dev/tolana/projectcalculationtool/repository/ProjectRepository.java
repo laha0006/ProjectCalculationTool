80b5d85368d55f9352ba6e454495059ca8064fb8
chye0001 <chye0001@stud.kea.dk>
2024-05-16 15:08:52 +0200

    Added ProjectRepository

--- src/main/java/dev/tolana/projectcalculationtool/repository/ProjectRepository.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/ProjectRepository.java
@@ -5,17 +5,5 @@ import dev.tolana.projectcalculationtool.model.Project;
 
 import java.util.List;
 
-public interface ProjectRepository {
-
-    int addProject(Project project);
-
-    List<Project> getAllProjectsOnUsername(String username);
-
-    long getTeamIdFromUsername(String username);
-
-    List<UserInformationDto> getTeamMembersFromTeamId(long teamId, long projectId);
-
-    void assignTeamMembersToProject(long projectId, List<String> selectedTeamMembers);
-
-    Project getProjectOnId(long projectId);
+public interface ProjectRepository extends ResourceEntityCrudOperations{
 }
