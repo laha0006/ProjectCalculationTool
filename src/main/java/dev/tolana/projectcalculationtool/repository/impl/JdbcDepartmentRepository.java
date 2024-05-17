ccb3f5bf1e7a6c101338bb9637f9354ddbfdcb09
chye0001 <chye0001@stud.kea.dk>
2024-05-17 10:38:53 +0200

    populated createEntity() in JdbcDepartmentRepository

--- src/main/java/dev/tolana/projectcalculationtool/repository/impl/JdbcDepartmentRepository.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/impl/JdbcDepartmentRepository.java
@@ -1,18 +1,17 @@
 package dev.tolana.projectcalculationtool.repository.impl;
 
+import dev.tolana.projectcalculationtool.dto.CreateDepartmentFormDto;
 import dev.tolana.projectcalculationtool.dto.UserInformationDto;
 import dev.tolana.projectcalculationtool.enums.UserRole;
 import dev.tolana.projectcalculationtool.model.Department;
 import dev.tolana.projectcalculationtool.model.Entity;
 import dev.tolana.projectcalculationtool.repository.DepartmentRepository;
-import dev.tolana.projectcalculationtool.repository.EntityCrudOperations;
+import dev.tolana.projectcalculationtool.util.RoleAssignUtil;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,7 +26,37 @@ public class JdbcDepartmentRepository implements DepartmentRepository {
 
     @Override
     public boolean createEntity(String username, Entity entity) {
-        return false;
+        boolean isCreated = false;
+
+        try (Connection connection = dataSource.getConnection()) {
+            try {
+                connection.setAutoCommit(false);
+
+                String createDepartment = "INSERT INTO department(name, description, organisation_id) VALUES (?, ?, ?)";
+                PreparedStatement pstmtAdd = connection.prepareStatement(createDepartment, Statement.RETURN_GENERATED_KEYS);
+                pstmtAdd.setString(1, entity.getName());
+                pstmtAdd.setString(2, entity.getDescription());
+                pstmtAdd.setLong(3, ((Department)entity).getOrganisationId());
+                int affectedRows = pstmtAdd.executeUpdate();
+                isCreated = affectedRows > 0;
+
+                ResultSet rs = pstmtAdd.getGeneratedKeys();
+                if(rs.next()) {
+                    long departmentId = rs.getLong(1);
+                    RoleAssignUtil.assignDepartmentRole(connection, departmentId, UserRole.DEPARTMENT_OWNER, username);
+                }
+
+                connection.commit();
+                connection.setAutoCommit(true);
+
+            } catch (Exception exception) {
+                connection.rollback();
+                connection.setAutoCommit(true);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return isCreated;
     }
 
     @Override
@@ -59,11 +88,11 @@ public class JdbcDepartmentRepository implements DepartmentRepository {
     }
 
     @Override
-    public List<Entity> getAllEntitiesOnId(long departmentId) {
+    public List<Entity> getAllEntitiesOnId(long organisationId) {
         List<Entity> departments = new ArrayList<>();
         try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from department where organisation_id = ?");
-            preparedStatement.setLong(1, departmentId);
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM department WHERE organisation_id = ?");
+            preparedStatement.setLong(1, organisationId);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 departments.add(new Department(
