55682cb5af3e366437fb38c6a0cb680bd5712dc7
Lars <laha0006@stud.kea.dk>
2024-05-17 05:33:27 +0200

    added and implemented invite related methods

--- src/main/java/dev/tolana/projectcalculationtool/service/UserService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/UserService.java
@@ -1,7 +1,9 @@
 package dev.tolana.projectcalculationtool.service;
 
+import dev.tolana.projectcalculationtool.dto.InviteFormDto;
 import dev.tolana.projectcalculationtool.dto.RegisterUserDto;
-import dev.tolana.projectcalculationtool.dto.inviteDto;
+import dev.tolana.projectcalculationtool.dto.InviteDto;
+import dev.tolana.projectcalculationtool.exception.UserAlreadyInOrganisationException;
 import dev.tolana.projectcalculationtool.repository.AuthorizationRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.User;
@@ -33,7 +35,7 @@ public class UserService {
         userDetailsManager.createUser(userToRegister);
     }
 
-    public List<inviteDto> getInvitations(String username) {
+    public List<InviteDto> getInvitations(String username) {
         return authorizationRepository.getInvitations(username);
     }
 
@@ -47,5 +49,28 @@ public class UserService {
 
     public void declineInvite(String username, long orgId) {
         authorizationRepository.removeInvite(username,orgId);
+    }
+
+    public boolean userInOrganisation(String username, long orgId) {
+        return authorizationRepository.checkUserInOrganisation(username,orgId) == 1;
+    }
+
+    @PreAuthorize("@auth.hasOrgansiationAccess(#formData.organisationId()," +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).ORGANISATION_INVITE)")
+    public void createInvite(InviteFormDto formData) {
+        String username = formData.username();
+        long organisationId = formData.organisationId();
+        if(userInOrganisation(username,organisationId)) {
+            throw new UserAlreadyInOrganisationException();
+        }
+        authorizationRepository.createInvite(username,organisationId);
+    }
+
+    @PreAuthorize("@auth.hasOrgansiationAccess(#formData.organisationId()," +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).ORGANISATION_INVITE)")
+    public void removeInvite(InviteFormDto formData) {
+        String username = formData.username();
+        long organisationId = formData.organisationId();
+        authorizationRepository.removeInvite(username,organisationId);
     }
 }
