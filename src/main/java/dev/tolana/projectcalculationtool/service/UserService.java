e6023f6c029e7a64b9e76c6fa6fc6a16e819ce4c
Lars <laha0006@stud.kea.dk>
2024-05-17 03:17:30 +0200

    implemented invite methods

--- src/main/java/dev/tolana/projectcalculationtool/service/UserService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/UserService.java
@@ -1,20 +1,27 @@
 package dev.tolana.projectcalculationtool.service;
 
 import dev.tolana.projectcalculationtool.dto.RegisterUserDto;
+import dev.tolana.projectcalculationtool.dto.inviteDto;
+import dev.tolana.projectcalculationtool.repository.AuthorizationRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
     private UserDetailsManager userDetailsManager;
     private PasswordEncoder passwordEncoder;
-    public UserService(UserDetailsManager userDetailsManager, PasswordEncoder passwordEncoder) {
+    private AuthorizationRepository authorizationRepository;
+    public UserService(UserDetailsManager userDetailsManager, PasswordEncoder passwordEncoder, AuthorizationRepository authorizationRepository) {
         this.userDetailsManager = userDetailsManager;
         this.passwordEncoder = passwordEncoder;
+        this.authorizationRepository = authorizationRepository;
     }
 
     public void registerUser(RegisterUserDto newUser) {
@@ -24,5 +31,17 @@ public class UserService {
                 .build();
 
         userDetailsManager.createUser(userToRegister);
+    }
+
+    public List<inviteDto> getInvitations(String username) {
+        return authorizationRepository.getInvitations(username);
+    }
+
+    public void acceptInvite(String username, long organisationId) {
+        authorizationRepository.addUserToOrganisation(username, organisationId);
+    }
+
+    public int getInvitationsCount(String username) {
+        return getInvitations(username).size();
     }
 }
