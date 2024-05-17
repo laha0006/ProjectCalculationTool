23de6f193a7d8e17bf48c1bb781cbd32d1b158dc
chye0001 <chye0001@stud.kea.dk>
2024-05-17 23:45:00 +0200

    cleaned up code

--- src/main/java/dev/tolana/projectcalculationtool/controller/OrganisationController.java
+++ src/main/java/dev/tolana/projectcalculationtool/controller/OrganisationController.java
@@ -2,21 +2,13 @@ package dev.tolana.projectcalculationtool.controller;
 
 import dev.tolana.projectcalculationtool.dto.EntityCreationDto;
 import dev.tolana.projectcalculationtool.dto.EntityViewDto;
-import dev.tolana.projectcalculationtool.dto.InviteDto;
-import dev.tolana.projectcalculationtool.dto.InviteFormDto;
 import dev.tolana.projectcalculationtool.enums.EntityType;
-import dev.tolana.projectcalculationtool.model.Entity;
-import dev.tolana.projectcalculationtool.model.Invitation;
-import dev.tolana.projectcalculationtool.model.Organisation;
 import dev.tolana.projectcalculationtool.service.DepartmentService;
 import dev.tolana.projectcalculationtool.service.OrganisationService;
-import dev.tolana.projectcalculationtool.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -25,11 +17,9 @@ import java.util.List;
 public class OrganisationController {
 
     private final OrganisationService organisationService;
-    private final DepartmentService departmentService;
 
-    public OrganisationController(OrganisationService organisationService, DepartmentService departmentService) {
+    public OrganisationController(OrganisationService organisationService) {
         this.organisationService = organisationService;
-        this.departmentService = departmentService;
     }
 
     @GetMapping("")
@@ -43,13 +33,14 @@ public class OrganisationController {
 
     @GetMapping("/{orgId}")
     public String organisationPage(@PathVariable("orgId") long organisationId, Model model) {
-        EntityViewDto organisation = organisationService.getOrganisationsById(organisationId);
-        List<EntityViewDto> departments = departmentService.getAll(organisationId);
+        EntityViewDto organisation = organisationService.getOrganisation(organisationId);
+        model.addAttribute("organisation", organisation);
+
+        List<EntityViewDto> departments = organisationService.getChildren(organisationId);
         if (departments.isEmpty()) {
             model.addAttribute("alertWarning", "Du er ikke medlem af nogle afdelinger!");
         }
         model.addAttribute("allDepartments", departments);
-        model.addAttribute("organisation", organisation);
         return "organisation/organisationView";
     }
 
