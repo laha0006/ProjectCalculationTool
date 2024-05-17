590161b59a22279981ea0e086a0fd46c7796a05b
Lars <laha0006@stud.kea.dk>
2024-05-17 03:20:25 +0200

    added inviteCount to global model, and exception handler method for InviteFailer. need to split in future.

--- src/main/java/dev/tolana/projectcalculationtool/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/tolana/projectcalculationtool/exception/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package dev.tolana.projectcalculationtool.exception;
 
 
+import dev.tolana.projectcalculationtool.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.SQLException;
@@ -15,6 +18,13 @@ public class GlobalExceptionHandler {
 //    this exception is thrown by JdbcUserDetailsManager
 //    which is the only class that will ever throw it.
 //    we know it's a 'username already exists' error, and send that back.
+
+    private UserService userService;
+
+    public GlobalExceptionHandler(UserService userService) {
+        this.userService = userService;
+    }
+
     @ExceptionHandler(DuplicateKeyException.class)
     public String sqlException(SQLException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("alertWarning", "username already exists!");
@@ -26,5 +36,21 @@ public class GlobalExceptionHandler {
         System.out.println("MyCustomException: " + ex.getMessage());
         String referer = request.getHeader("Referer");
         return "redirect:" + referer;
+    }
+
+    @ExceptionHandler(InviteFailureException.class)
+    public String inviteFailureException(InviteFailureException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        String referer = request.getHeader("Referer");
+        redirectAttributes.addFlashAttribute("alertWarning", ex.getMessage());
+        return "redirect:" + referer;
+    }
+
+    @ModelAttribute("inviteCount")
+    public int getInviteCount(Authentication authentication) {
+        if (authentication == null) {
+            return 0;
+        }
+        String username = authentication.getName();
+        return userService.getInvitationsCount(username);
     }
 }
