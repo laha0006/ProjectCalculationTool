0462af5309ae964188e6e13be8b1ba2c34098161
Lars <laha0006@stud.kea.dk>
2024-05-17 05:32:27 +0200

    added method to interface

--- src/main/java/dev/tolana/projectcalculationtool/repository/OrganisationRepository.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/OrganisationRepository.java
@@ -1,4 +1,9 @@
 package dev.tolana.projectcalculationtool.repository;
 
+import dev.tolana.projectcalculationtool.model.Invitation;
+
+import java.util.List;
+
 public interface OrganisationRepository extends EntityCrudOperations{
+    List<Invitation> getAllOutstandingInvitations(long organisationId);
 }
