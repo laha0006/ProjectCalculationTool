22eb4ce881782851858c6b0bec1b0b5f4859c794
chye0001 <chye0001@stud.kea.dk>
2024-05-18 00:08:31 +0200

    refactor ProjectService

--- src/main/java/dev/tolana/projectcalculationtool/service/ProjectService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/ProjectService.java
@@ -1,17 +1,16 @@
 package dev.tolana.projectcalculationtool.service;
 
 import dev.tolana.projectcalculationtool.dto.ProjectOverviewDto;
+import dev.tolana.projectcalculationtool.dto.ResourceEntityViewDto;
 import dev.tolana.projectcalculationtool.dto.UserInformationDto;
 import dev.tolana.projectcalculationtool.enums.UserRole;
 import dev.tolana.projectcalculationtool.mapper.EntityDtoMapper;
 import dev.tolana.projectcalculationtool.model.Entity;
 import dev.tolana.projectcalculationtool.model.Project;
 import dev.tolana.projectcalculationtool.repository.ProjectRepository;
-import dev.tolana.projectcalculationtool.repository.ResourceEntityCrudOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class ProjectService {
@@ -24,14 +23,19 @@ public class ProjectService {
         this.entityDtoMapper = entityDtoMapper;
     }
 
-    public void addProject(String username, Entity project) {
+    public ResourceEntityViewDto getProject(long projectId) {
+        Entity project = projectRepository.getEntityOnId(projectId);
+        return entityDtoMapper.toResourceEntityViewDto(project);
+    }
+
+    public void createProject(String username, Entity project) {
         projectRepository.createEntity(username, project);
     }
 
-    public List<ProjectOverviewDto> getAllProjects(String username) {
-        List<Entity> projectList = projectRepository.getAllEntitiesOnUsername(username);
-        return entityDtoMapper.toProjectOverviewDtoList(projectList);
-    }
+//    public List<ProjectOverviewDto> getAllProjects(String username) {
+//        List<Entity> projectList = projectRepository.getAllEntitiesOnUsername(username);
+//        return entityDtoMapper.toProjectOverviewDtoList(projectList);
+//    }
 
     public List<UserInformationDto> getAllTeamMembersFromTeamId(long teamId) {
         return projectRepository.getUsersFromEntityId(teamId);
@@ -41,16 +45,14 @@ public class ProjectService {
         projectRepository.assignUser(projectId, selectedTeamMembers, userRole);
     }
 
-    public ProjectOverviewDto getProjectOnId(long projectId) {
-        Entity project = projectRepository.getEntityOnId(projectId);
-        return entityDtoMapper.toProjectOverviewDto((Project) project);
-    }
-
     public List<UserRole> getAllUserRoles() {
         return projectRepository.getAllUserRoles();
     }
 
     public void deleteProject(long projectId) {
         projectRepository.deleteEntity(projectId);
+    }
+
+    public List<ResourceEntityViewDto> getChildren(long projectId) {
     }
 }
