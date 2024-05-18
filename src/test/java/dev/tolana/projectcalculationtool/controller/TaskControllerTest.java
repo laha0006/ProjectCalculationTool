d53f8203faae340487309064055e795c838c0165
chye0001 <chye0001@stud.kea.dk>
2024-05-18 21:57:22 +0200

    removed unused import statement

--- src/test/java/dev/tolana/projectcalculationtool/controller/TaskControllerTest.java
+++ src/test/java/dev/tolana/projectcalculationtool/controller/TaskControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
