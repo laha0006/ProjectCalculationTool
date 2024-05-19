8a4459c6e49da92612ebca1f8112c8a7a70f3319
Lars <laha0006@stud.kea.dk>
2024-05-19 13:58:21 +0200

    refactored test to reflect changes

--- src/test/java/dev/tolana/projectcalculationtool/controller/ProjectControllerTest.java
+++ src/test/java/dev/tolana/projectcalculationtool/controller/ProjectControllerTest.java
@@ -6,7 +6,9 @@ import dev.tolana.projectcalculationtool.enums.Status;
 import dev.tolana.projectcalculationtool.service.ProjectService;
 import dev.tolana.projectcalculationtool.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.ParameterResolutionDelegate;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -14,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -40,7 +43,7 @@ class ProjectControllerTest {
         when(projectService.getProject(1))
                 .thenReturn(new ResourceEntityViewDto("name", "description", 1, 1, 1, 1, LocalDateTime.now(), 1, 1, 1, Status.TODO));
         when(projectService.getProjectStats(1))
-                .thenReturn(new ProjectStatsDto(100,100,100));
+                .thenReturn(new ProjectStatsDto(1,1,1,1,1,1,,1,1,new HashMap<>(),new HashMap<>()));
         mockMvc.perform(get("/organisation/{orgId}/department/{deptId}/team/{teamId}/project/{projectId}", 1, 1, 1, 1))
                 .andExpect(status().isOk())
                 .andExpect(view().name("project/projectView"));
