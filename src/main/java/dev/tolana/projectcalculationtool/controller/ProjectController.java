e0d65d955a177df910081e1c4ef92a42117091f5
chye0001 <chye0001@stud.kea.dk>
2024-05-19 20:00:48 +0200

    added attribute int allottedHours to empty ProjectCreationDto

--- src/main/java/dev/tolana/projectcalculationtool/controller/ProjectController.java
+++ src/main/java/dev/tolana/projectcalculationtool/controller/ProjectController.java
@@ -53,7 +53,7 @@ public class ProjectController {
                                            @PathVariable long orgId,
                                            @PathVariable long deptId,
                                            @PathVariable long teamId) {
-        model.addAttribute("newProject",new ProjectCreationDto("", "", -1, teamId, LocalDateTime.now()));
+        model.addAttribute("newProject", new ProjectCreationDto("", "", -1, teamId, LocalDateTime.now(), 0));
         model.addAttribute("orgId", orgId);
         model.addAttribute("deptId", deptId);
         //TODO add something that makes it possible to display Team/Department/Organization/whatever
@@ -67,7 +67,7 @@ public class ProjectController {
                                              @PathVariable long deptId,
                                              @PathVariable long teamId,
                                              @PathVariable long projectId) {
-        model.addAttribute("newProject",new ProjectCreationDto("", "", projectId, teamId, LocalDateTime.now()));
+        model.addAttribute("newProject", new ProjectCreationDto("", "", projectId, teamId, LocalDateTime.now(), 0));
         model.addAttribute("orgId", orgId);
         model.addAttribute("deptId", deptId);
 
