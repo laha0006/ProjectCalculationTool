4830194797c3b0f12e0c4645eec04b0b4730215f
chye0001 <chye0001@stud.kea.dk>
2024-05-19 16:27:25 +0200

    added long parentId to ProjectCreationDto

--- src/main/java/dev/tolana/projectcalculationtool/dto/ProjectCreationDto.java
+++ src/main/java/dev/tolana/projectcalculationtool/dto/ProjectCreationDto.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 public record ProjectCreationDto(
         String projectName,
         String description,
+        long parentId,
         long teamId,
         LocalDateTime deadline
 ) {
