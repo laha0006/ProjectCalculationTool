977d3dadf52e24d047cab5c0df1d21e5e7f45ecf
Lars <laha0006@stud.kea.dk>
2024-05-21 01:59:15 +0200

    removed obsolete method

--- src/main/java/dev/tolana/projectcalculationtool/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/tolana/projectcalculationtool/exception/GlobalExceptionHandler.java
@@ -31,13 +31,6 @@ public class GlobalExceptionHandler {
         return "redirect:/user/register";
     }
 
-    @ExceptionHandler(MyCustomException.class)
-    public String myCustomException(MyCustomException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
-        System.out.println("MyCustomException: " + ex.getMessage());
-        String referer = request.getHeader("Referer");
-        return "redirect:" + referer;
-    }
-
     @ExceptionHandler(InviteFailureException.class)
     public String inviteFailureException(InviteFailureException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
         String referer = request.getHeader("Referer");
