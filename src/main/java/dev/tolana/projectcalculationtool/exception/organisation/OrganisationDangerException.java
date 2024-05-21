9456315a52252f3db78ab510bd24fc7b5ee672b1
Lars <laha0006@stud.kea.dk>
2024-05-21 02:54:04 +0200

    made DangerException for organisation.

---
+++ src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationDangerException.java
@@ -0,0 +1,7 @@
+package dev.tolana.projectcalculationtool.exception.organisation;
+
+public class OrganisationDangerException extends RuntimeException {
+    public OrganisationDangerException(String message) {
+        super(message);
+    }
+}
