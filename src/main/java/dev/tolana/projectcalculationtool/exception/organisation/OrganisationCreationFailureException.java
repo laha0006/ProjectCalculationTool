4f72e9af19330ceb92576ee7c37208b54d461a60
Lars <laha0006@stud.kea.dk>
2024-05-21 02:35:09 +0200

    renamed.

--- src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationCreationFailureException.java
+++
@@ -1,7 +0,0 @@
-package dev.tolana.projectcalculationtool.exception.organisation;
-
-public class OrganisationCreationFailureException extends RuntimeException {
-    public OrganisationCreationFailureException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationException.java
@@ -0,0 +1,7 @@
+package dev.tolana.projectcalculationtool.exception.organisation;
+
+public class OrganisationException extends RuntimeException {
+    public OrganisationException(String message) {
+        super(message);
+    }
+}
