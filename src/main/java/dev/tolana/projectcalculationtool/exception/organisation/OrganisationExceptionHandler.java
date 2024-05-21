0d1a79ab40c32113b409438b579c58d3b92b0075
Lars <laha0006@stud.kea.dk>
2024-05-21 02:35:37 +0200

    implemented exception handler method.

--- src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationExceptionHandler.java
+++ src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationExceptionHandler.java
@@ -1,7 +1,17 @@
 package dev.tolana.projectcalculationtool.exception.organisation;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class OrganisationExceptionHandler {
+
+    @ExceptionHandler(OrganisationException.class)
+    public String organisationCreationFailureException(OrganisationException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        String referer = request.getHeader("referer");
+        redirectAttributes.addFlashAttribute(ex.getMessage());
+        return "redirect:" + referer;
+    }
 }
