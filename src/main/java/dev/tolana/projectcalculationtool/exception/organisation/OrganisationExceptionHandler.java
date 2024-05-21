70b86880d970b0c27089022a81a8fe0e6911ae35
Lars <laha0006@stud.kea.dk>
2024-05-21 03:21:39 +0200

    handler now correctly implements EntityExceptionHandler.

--- src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationExceptionHandler.java
+++ src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationExceptionHandler.java
@@ -1,22 +1,25 @@
 package dev.tolana.projectcalculationtool.exception.organisation;
 
+import dev.tolana.projectcalculationtool.exception.EntityException;
+import dev.tolana.projectcalculationtool.exception.EntityExceptionHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
-public class OrganisationExceptionHandler {
+public class OrganisationExceptionHandler implements EntityExceptionHandler {
 
+    @Override
     @ExceptionHandler(OrganisationWarningException.class)
-    public String organisationWarningException(OrganisationWarningException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    public String warningException(EntityException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
         String referer = request.getHeader("referer");
         redirectAttributes.addFlashAttribute("alertWarning",ex.getMessage());
         return "redirect:" + referer;
     }
-
+    @Override
     @ExceptionHandler(OrganisationDangerException.class)
-    public String organisationDangerException(OrganisationWarningException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    public String dangerException(EntityException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
         String referer = request.getHeader("referer");
         redirectAttributes.addFlashAttribute("alertDanger",ex.getMessage());
         return "redirect:" + referer;
