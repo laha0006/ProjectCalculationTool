6bdd5edd3faf33b4fefa56699f75a794840798fa
Lars <laha0006@stud.kea.dk>
2024-05-21 02:24:53 +0200

    created a handler for all organisation related exceptions.

---
+++ src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationExceptionHandler.java
@@ -0,0 +1,7 @@
+package dev.tolana.projectcalculationtool.exception.organisation;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class OrganisationExceptionHandler {
+}
