6cc5f3cf42971a72fc9f55e46c1109470d08d9e1
Lars <laha0006@stud.kea.dk>
2024-05-21 03:33:58 +0200

    refactor

--- src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationExceptionHandler.java
+++ src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationExceptionHandler.java
@@ -1,5 +1,6 @@
 package dev.tolana.projectcalculationtool.exception.organisation;
 
+import dev.tolana.projectcalculationtool.enums.Alert;
 import dev.tolana.projectcalculationtool.exception.EntityException;
 import dev.tolana.projectcalculationtool.exception.EntityExceptionHandler;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,20 +9,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
-public class OrganisationExceptionHandler implements EntityExceptionHandler {
+public class OrganisationExceptionHandler{
 
-    @Override
     @ExceptionHandler(OrganisationWarningException.class)
-    public String warningException(EntityException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    public String handle(EntityException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
         String referer = request.getHeader("referer");
-        redirectAttributes.addFlashAttribute("alertWarning",ex.getMessage());
+        switch(ex.getAlert()) {
+            case WARNING:
+                redirectAttributes.addFlashAttribute("alertWarning", ex.getMessage());
+            case DANGER:
+                redirectAttributes.addFlashAttribute("alertDanger", ex.getMessage());
+        }
         return "redirect:" + referer;
     }
-    @Override
-    @ExceptionHandler(OrganisationDangerException.class)
-    public String dangerException(EntityException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
-        String referer = request.getHeader("referer");
-        redirectAttributes.addFlashAttribute("alertDanger",ex.getMessage());
-        return "redirect:" + referer;
-    }
+
 }
