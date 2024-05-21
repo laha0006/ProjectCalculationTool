2134d426c282d3417a38fe9109f029be64fc2166
chye0001 <chye0001@stud.kea.dk>
2024-05-21 18:34:52 +0200

    created taskCreation-, Edit-, and ViewDto

---
+++ src/main/java/dev/tolana/projectcalculationtool/dto/TaskEditDto.java
@@ -0,0 +1,19 @@
+package dev.tolana.projectcalculationtool.dto;
+
+import dev.tolana.projectcalculationtool.enums.Status;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+public record TaskEditDto (
+        String taskName,
+        String description,
+        long parentId,
+
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+        LocalDateTime deadline,
+        int estimatedHours,
+        int actualHours,
+        Status status
+){
+}
