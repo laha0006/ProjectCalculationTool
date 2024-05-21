1c2059294303d4aed8a59b03278959230ab0b7f2
Lars <laha0006@stud.kea.dk>
2024-05-21 02:24:34 +0200

    implemented OrganisationCreationFailureexception

---
+++ src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationCreationFailureException.java
@@ -0,0 +1,7 @@
+package dev.tolana.projectcalculationtool.exception.organisation;
+
+public class OrganisationCreationFailureException extends RuntimeException {
+    public OrganisationCreationFailureException(String message) {
+        super(message);
+    }
+}
