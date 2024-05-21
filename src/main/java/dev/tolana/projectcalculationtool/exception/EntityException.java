282fc2dd2512bff0ae327399b9d7d512c8e037e1
Lars <laha0006@stud.kea.dk>
2024-05-21 03:25:57 +0200

    updated EntityException to use alert field.

--- src/main/java/dev/tolana/projectcalculationtool/exception/EntityException.java
+++ src/main/java/dev/tolana/projectcalculationtool/exception/EntityException.java
@@ -1,7 +1,11 @@
 package dev.tolana.projectcalculationtool.exception;
 
+import dev.tolana.projectcalculationtool.enums.Alert;
+
 public class EntityException extends RuntimeException{
-    public EntityException(String message) {
+    private final Alert alert;
+    public EntityException(String message, Alert alert) {
         super(message);
+        this.alert = alert;
     }
 }
