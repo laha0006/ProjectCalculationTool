2821ae548d65e5970d5eccc3b52fdc805e618d1c
Lars <laha0006@stud.kea.dk>
2024-05-21 02:04:12 +0200

    deleted - not used.

--- src/main/java/dev/tolana/projectcalculationtool/exception/MyCustomException.java
+++
@@ -1,9 +0,0 @@
-package dev.tolana.projectcalculationtool.exception;
-
-import java.sql.SQLException;
-
-public class MyCustomException extends SQLException {
-    public MyCustomException(String reason) {
-        super(reason);
-    }
-}
