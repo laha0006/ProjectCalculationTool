c5dce0d2926c9083692b03d55c836bfa6a92bf1c
chye0001 <chye0001@stud.kea.dk>
2024-05-21 20:16:10 +0200

    removed unused mapping methods

--- src/main/java/dev/tolana/projectcalculationtool/mapper/EntityDtoMapper.java
+++ src/main/java/dev/tolana/projectcalculationtool/mapper/EntityDtoMapper.java
@@ -132,19 +132,6 @@ public class EntityDtoMapper {
         );
     }
 
-    public Entity toEntity(TaskDto taskDto) {
-        return new Task(
-                taskDto.taskId(),
-                taskDto.taskName(),
-                taskDto.taskDescription(),
-                taskDto.deadline(),
-                taskDto.status(),
-                taskDto.parentId(),
-                taskDto.projectId(),
-                taskDto.estimatedHours()
-        );
-    }
-
     public Entity toEntity(EntityEditDto editDto) {
         return new Entity(
                 editDto.id(),
@@ -315,39 +302,5 @@ public class EntityDtoMapper {
             }
         }
         return entityViewDtoList;
-    }
-
-    public List<TaskDto> toTaskDtoList(List<Entity> taskList) {
-        List<TaskDto> taskDtoList = new ArrayList<>();
-
-        for (Entity task : taskList) {
-            TaskDto taskDto = new TaskDto(
-                    task.getName(),
-                    task.getDescription(),
-                    ((Task) task).getProjectId(),
-                    ((ResourceEntity) task).getDeadline(),
-                    ((Task) task).getEstimatedHours(),
-                    ((ResourceEntity) task).getStatus(),
-                    ((ResourceEntity) task).getParentId(),
-                    task.getId()
-            );
-            taskDtoList.add(taskDto);
-        }
-
-        return taskDtoList;
-    }
-
-    public TaskDto convertToTaskDto(Task task) {
-
-        String taskName = task.getName();
-        String taskDescription = task.getDescription();
-        long projectId = task.getProjectId();
-        LocalDateTime deadline = task.getDeadline();
-        int estimatedHours = task.getEstimatedHours();
-        Status status = task.getStatus();
-        long parentId = task.getParentId();
-        long taskId = task.getId();
-
-        return new TaskDto(taskName, taskDescription, projectId, deadline, estimatedHours, status, parentId, taskId);
     }
 }
