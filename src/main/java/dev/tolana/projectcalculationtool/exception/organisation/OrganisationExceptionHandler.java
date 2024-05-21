e834b4b4d72499897db9f89af3b52466cb77a91f
Lars <laha0006@stud.kea.dk>
2024-05-21 02:54:22 +0200

    refactor due to rename of exception

--- src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationExceptionHandler.java
+++ src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationExceptionHandler.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @ControllerAdvice
 public class OrganisationExceptionHandler {
 
-    @ExceptionHandler(OrganisationException.class)
-    public String organisationCreationFailureException(OrganisationException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    @ExceptionHandler(OrganisationWarningException.class)
+    public String organisationCreationFailureException(OrganisationWarningException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
         String referer = request.getHeader("referer");
         redirectAttributes.addFlashAttribute("alertWarning",ex.getMessage());
         return "redirect:" + referer;
