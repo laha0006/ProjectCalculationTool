72911a75a8783d724845ef03732631d2bc2a6e94
Lars <laha0006@stud.kea.dk>
2024-05-21 03:44:02 +0200

    moved

--- src/main/java/dev/tolana/projectcalculationtool/exception/OrganisationException.java
+++ src/main/java/dev/tolana/projectcalculationtool/exception/OrganisationException.java
@@ -1,10 +1,8 @@
-package dev.tolana.projectcalculationtool.exception.organisation;
+package dev.tolana.projectcalculationtool.exception;
 
 import dev.tolana.projectcalculationtool.enums.Alert;
-import dev.tolana.projectcalculationtool.exception.EntityException;
 
 public class OrganisationException extends EntityException {
     public OrganisationException(String message, Alert alert) {
         super(message, alert);
-    }
-}
+    }}
