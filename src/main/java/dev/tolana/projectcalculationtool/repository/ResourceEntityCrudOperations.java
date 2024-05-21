d996859be2827fc2cd70b9c146f604cc28de4905
chye0001 <chye0001@stud.kea.dk>
2024-05-21 19:48:58 +0200

    removed changeStatus()

--- src/main/java/dev/tolana/projectcalculationtool/repository/ResourceEntityCrudOperations.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/ResourceEntityCrudOperations.java
@@ -6,7 +6,5 @@ import java.util.List;
 
 public interface ResourceEntityCrudOperations extends EntityCrudOperations {
 
-    boolean changeStatus(long resourceEntityId);
-
     List<Status> getStatusList();
 }
