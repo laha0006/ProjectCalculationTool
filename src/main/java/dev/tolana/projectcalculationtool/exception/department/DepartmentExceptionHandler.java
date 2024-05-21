e69720b26c5466f467798c57c52fe09769cafd15
Lars <laha0006@stud.kea.dk>
2024-05-21 03:43:37 +0200

    deleted obsolete exception

--- src/main/java/dev/tolana/projectcalculationtool/exception/department/DepartmentExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package dev.tolana.projectcalculationtool.exception.department;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class DepartmentExceptionHandler {
-}
