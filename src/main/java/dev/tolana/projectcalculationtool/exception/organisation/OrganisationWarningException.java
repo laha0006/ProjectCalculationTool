7430088f85c5f2a4659dcce0bcf28d83aa7abe57
Lars <laha0006@stud.kea.dk>
2024-05-21 03:40:26 +0200

    deleted obsolete exception

--- src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationWarningException.java
+++
@@ -1,7 +0,0 @@
-package dev.tolana.projectcalculationtool.exception.organisation;
-
-public class OrganisationWarningException extends RuntimeException {
-    public OrganisationWarningException(String message) {
-        super(message);
-    }
-}
