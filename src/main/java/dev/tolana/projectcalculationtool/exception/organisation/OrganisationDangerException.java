89ea0db170ddd922dac8fb6d195d5dc29bf3463c
Lars <laha0006@stud.kea.dk>
2024-05-21 03:40:23 +0200

    deleted obsolete exception

--- src/main/java/dev/tolana/projectcalculationtool/exception/organisation/OrganisationDangerException.java
+++
@@ -1,9 +0,0 @@
-package dev.tolana.projectcalculationtool.exception.organisation;
-
-import dev.tolana.projectcalculationtool.exception.EntityException;
-
-public class OrganisationDangerException extends EntityException {
-    public OrganisationDangerException(String message) {
-        super(message);
-    }
-}
