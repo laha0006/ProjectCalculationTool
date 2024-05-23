0e18f69cf289112b7f6f32ded055d86f445a7f29
Lars <laha0006@stud.kea.dk>
2024-05-24 01:04:06 +0200

    breadcrumb wip

--- src/test/java/dev/tolana/projectcalculationtool/MysqlConnectionTest.java
+++ src/test/java/dev/tolana/projectcalculationtool/MysqlConnectionTest.java
@@ -20,6 +20,30 @@ public class MysqlConnectionTest {
     private DataSource dataSource;
 
     @Test
+    public void testMysqlConnection() throws SQLException {
+        String url1 = "/organisation/6/department/5/team/3/project/6/task/4";
+        String url2 = "/organisation/6/department/5/team/3/project/6";
+        String url3 = "/organisation/6/department/5/team/3";
+        String url4 = "/organisation/6/department/5";
+        String url5 = url1.substring(1);
+        String[] tokens = url5.split("/");
+        for (String token : tokens) {
+            System.out.println("token: " + token);
+        }
+        int length = tokens.length;
+        System.out.println("length: " + length);
+        switch(length) {
+            case 2 -> System.out.println("org");
+            case 4 -> System.out.println("department");
+            case 6 -> System.out.println("team");
+            case 8 -> System.out.println("project");
+            case 10 -> System.out.println("task");
+        }
+
+    }
+
+
+    @Test
     void testConnection() {
 
         try(Connection con = dataSource.getConnection()) {
