98e9d0e09fc50578b09a5a093b307f2f79c1ecd7
chye0001 <chye0001@stud.kea.dk>
2024-05-24 14:49:15 +0200

    added class JdbcProjectRepositoryTest

---
+++ src/test/java/dev/tolana/projectcalculationtool/repository/JdbcProjectRepositoryTest.java
@@ -0,0 +1,36 @@
+package dev.tolana.projectcalculationtool.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JdbcProjectRepositoryTest {
+
+    @Test
+    void createEntity() {
+    }
+
+    @Test
+    void getEntityOnId() {
+    }
+
+    @Test
+    void getChildren() {
+    }
+
+    @Test
+    void getParent() {
+    }
+
+    @Test
+    void getSubProjects() {
+    }
+
+    @Test
+    void editEntity() {
+    }
+
+    @Test
+    void deleteEntity() {
+    }
+}
