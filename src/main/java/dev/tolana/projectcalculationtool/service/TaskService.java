91613eb55c4bcae622dcf26adb5b1972719f52e3
Lars <laha0006@stud.kea.dk>
2024-05-28 23:44:08 +0200

    added method security.

--- src/main/java/dev/tolana/projectcalculationtool/service/TaskService.java
+++ src/main/java/dev/tolana/projectcalculationtool/service/TaskService.java
@@ -8,6 +8,8 @@ import dev.tolana.projectcalculationtool.enums.Status;
 import dev.tolana.projectcalculationtool.mapper.TaskDtoMapper;
 import dev.tolana.projectcalculationtool.model.Entity;
 import dev.tolana.projectcalculationtool.repository.TaskRepository;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,25 +27,36 @@ public class TaskService {
         this.taskDtoMapper = taskDtoMapper;
     }
 
+
+    @PreAuthorize("@auth.hasProjectAccess(#newTask.parentId(), " +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).TASK_CREATE)")
     public void createTask(String username, TaskCreationDto newTask) {
         Entity task =  taskDtoMapper.toEntity(newTask);
         taskRepository.createEntity(username, task);
     }
 
+    @PreAuthorize("@auth.hasTaskAccess(#taskId, " +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).TASK_READ)")
     public TaskViewDto getTaskToView(long taskId) {
         Entity task = taskRepository.getEntityOnId(taskId);
         return taskDtoMapper.toTaskViewDto(task);
     }
 
+    @PreAuthorize("@auth.hasTaskAccess(#taskId, " +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).TASK_EDIT)")
     public TaskEditDto getTaskToEdit(long taskId) {
         Entity task = taskRepository.getEntityOnId(taskId);
         return taskDtoMapper.toTaskEditDto(task);
     }
 
+    @PreAuthorize("@auth.hasTaskAccess(#taskId, " +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).TASK_DELETE)")
     public void deleteTask(long taskId) {
         taskRepository.deleteEntity(taskId);
     }
 
+    @PostFilter("@auth.hasTaskAccess(filterObject.id(), " +
+                "T(dev.tolana.projectcalculationtool.enums.Permission).TASK_READ)")
     public List<TaskViewDto> getChildren(long taskId) {
         List<Entity> taskList = taskRepository.getChildren(taskId);
         return taskDtoMapper.toTaskDtoViewList(taskList);
@@ -53,6 +66,9 @@ public class TaskService {
         return taskRepository.getStatusList();
     }
 
+
+    @PreAuthorize("@auth.hasTaskAccess(#task.id(), " +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).TASK_EDIT)")
     public void editTask(TaskEditDto task) {
         Entity taskToEdit = taskDtoMapper.toEntity(task);
         taskRepository.editEntity(taskToEdit);
@@ -89,15 +105,25 @@ public class TaskService {
         return scrubbedUsers;
     }
 
+    @PreAuthorize("@auth.hasTaskAccess(#taskId, " +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).TASK_READ)") // task_read is sufficient since self-assignment is allowed.
     public void assignMemberToTask(String username, long taskId) {
         taskRepository.assignMemberToEntity(taskId, username);
     }
 
+    @PreAuthorize("@auth.hasTaskAccess(#taskId, " +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).TASK_KICK)")
     public void kickMemberFromTask(long taskId, String username) {
         taskRepository.kickMember(taskId, username);
     }
 
-    public void promoteMemberToAdmin(long projectId, String username) {
-        taskRepository.promoteMemberToAdmin(projectId, username);
+    public void removeSelfFromTask(long taskId) {
+        taskRepository.removeSelfFromTask(taskId);
+    }
+
+    @PreAuthorize("@auth.hasTaskAccess(#taskId, " +
+                  "T(dev.tolana.projectcalculationtool.enums.Permission).TASK_EDIT)")
+    public void promoteMemberToAdmin(long taskId, String username) {
+        taskRepository.promoteMemberToAdmin(taskId, username);
     }
 }
