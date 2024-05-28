c30119cd2daeba4ebb9d1593dc50f080dd051b7a
Lars <laha0006@stud.kea.dk>
2024-05-28 21:19:52 +0200

    added FAQ and About endpoint mappings

--- src/main/java/dev/tolana/projectcalculationtool/controller/IndexController.java
+++ src/main/java/dev/tolana/projectcalculationtool/controller/IndexController.java
@@ -2,7 +2,18 @@ package dev.tolana.projectcalculationtool.controller;
 
 import dev.tolana.projectcalculationtool.service.DashboardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
+    @GetMapping("/faq")
+    public String faq() {
+        return "faq";
+    }
+
+    @GetMapping("about")
+    public String about() {
+        return "about";
+    }
 }
