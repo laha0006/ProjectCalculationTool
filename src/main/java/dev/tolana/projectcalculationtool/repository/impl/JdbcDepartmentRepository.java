2f8bfb3dec78a07e61842ac4cceba11dbc2594fe
Lars <laha0006@stud.kea.dk>
2024-05-28 16:28:56 +0200

    refactored

--- src/main/java/dev/tolana/projectcalculationtool/repository/impl/JdbcDepartmentRepository.java
+++ src/main/java/dev/tolana/projectcalculationtool/repository/impl/JdbcDepartmentRepository.java
@@ -237,7 +237,7 @@ public class JdbcDepartmentRepository implements DepartmentRepository {
     }
 
     @Override
-    public List<UserInformationDto> getUsersFromEntityId(long entityId) {
+    public List<UserEntityRoleDto> getUsersFromEntityId(long entityId) {
         return null;
     }
 
